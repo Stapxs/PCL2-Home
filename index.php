3da851a926375790b10530b76cbb0459e249874e
Stapx Steve <1007028430.stapx@gmail.com>
2021-02-05 22:12:18 +0800

    New | index

---
+++ index.php
@@ -0,0 +1,38 @@
+<?php include_once('main/header.html'); ?>
+
+<div id="main" style="margin: 0 auto;" class="card-main">
+    <div class="shadow-sm p-3 mb-5 bg-white rounded">
+        <div class="card-body">
+            <div class="jumbotron" style="background: transparent;padding: 0;margin: 0">
+                <h1 class="display-5">PCL2 主页生成 2.0</h1>
+                <p class="lead">这是个模块化可自定义的 PCL2 主页生成 API 2.0，此 API 的前身是我写的另一个固定样式 API。</p>
+                <hr class="my-4">
+                <p>本 API 已在 GitHub 开源，喜欢记得点个 Star！</p>
+                <a class="btn btn-primary btn-lg" href="https://github.com/Stapxs/PCL2-Home" role="button">去看看</a>
+            </div>
+        </div>
+        <div style="clear:both;">
+    </div>
+</div>
+
+<div class="shadow-sm p-3 mb-5 bg-white rounded" style="padding: 0 !important;">
+    <div class="alert alert-primary" role="alert">
+        感谢使用 PCL2 主页生成 2.0 ！下面是简单的介绍和使用指导！
+    </div>
+</div>
+
+<div>
+    <div class="shadow-sm p-3 mb-5 bg-white rounded about-left">
+        <img src="img/PCL2-Home.png"  alt="PCL2 Home"/>
+    </div>
+    <div class="shadow-sm p-3 mb-5 bg-white rounded about-right">
+        PCL2 提供了一个功能简单但是自定义丰富的 XAML 首页更改功能，同时提供了在线获取主页 XAML 代码的功能。<br>
+        本 API 主要提供本地 XAML 无法做到的在线部分功能，通过请求数据、拼接结果等方式来实现半动态的 PCL2 主页功能。
+    </div>
+</div>
+
+<div class="shadow-sm p-3 mb-5 bg-white rounded">
+    懒得写了，回头再补。
+</div>
+
+<?php include_once('main/footer.html'); ?>
